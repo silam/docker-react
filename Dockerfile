1b4815d2f121c63179c3b1c945c3118335f87334
silam <silam@hotmail.com>
2020-07-30 22:21:14 -0500

    First commit

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM node:alpine as Builder 
+
+WORKDIR '/app'
+
+COPY package.json .
+
+RUN npm install
+
+COPY . .
+
+RUN npm run build
+
+
+
+
+
+
+FROM nginx
+
+COPY --from=Builder  /app/build /usr/share/nginx/html
+
--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+            Bye There!
+          </p>
         <a
           className="App-link"
           href="https://reactjs.org"
